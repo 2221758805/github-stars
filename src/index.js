7248ee8903cb0e92153c1a158a98afebf0002011
sorrycc <sorrycc@gmail.com>
2017-01-04 13:27:36 +0800

    upgrade-to-dva: add entry and router files

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-import 'babel-polyfill';
-import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
-import { syncReduxAndRouter } from 'react-router-redux';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import dva from 'dva';
 import pick from 'lodash.pick';
-
-import App from './containers/App';
-import configure from './store/index';
 
 const data = localStorage.getItem('github');
 const initialState = data ? JSON.parse(data) : {};
-const store = configure(initialState);
-store.subscribe(() => {
-  localStorage.setItem('github', JSON.stringify(pick(store.getState(), ['stars', 'user'])));
+
+const app = dva({
+  initialState,
+  onStateChange() {
+    localStorage.setItem('github', JSON.stringify(pick(app._store.getState(), ['stars', 'user'])));
+  },
 });
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={App} />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+app.router(require('./router'));
+
+app.start('#root');
